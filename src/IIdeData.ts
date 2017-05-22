53286871f20f1d3d1cd79b215a403e3955cff8cb
dymo <jais@pedersens.net>
2017-05-22 12:21:10 +0700

    Fix #7 c_cpp_properties.json should be recreated

---
+++ src/IIdeData.ts
@@ -0,0 +1,11 @@
+export interface IIdeData {
+    cxx_path: string;
+    cc_flags: string;
+    prog_path: string;
+    gdb_path: string;
+    cxx_flags: string;
+    includes: string[];
+    libsource_dirs: string[];
+    cc_path: string;
+    defines: string[];
+}
